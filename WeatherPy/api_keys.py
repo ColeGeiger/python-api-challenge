b36d0ae88deceaed344f0e87946f6fc5234b54b9
ColeGeiger <49625195+ColeGeiger@users.noreply.github.com>
2023-11-10 13:23:05 -0700

    Generated code for WeatherPy and VacationPy
    generated all the code need for both py files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d3ec7b14432f866814861f50106af03c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "90a0313602f646c2a119b905131cd618"
